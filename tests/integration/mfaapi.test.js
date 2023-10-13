4574226858a758899f26c0dd8df168f4f5c9c16a
ckoegel <ckoegel1006@gmail.com>
2023-10-13 15:01:50 -0400

    mfa tests

---
+++ tests/integration/mfaapi.test.js
@@ -0,0 +1,60 @@
+const { MFAApi } = require('../../api/mfaapi');
+const { Configuration } = require('../../configuration');
+
+describe('MFAApi', () => {
+    const config = new Configuration({username: BW_USERNAME, password: BW_PASSWORD});
+    const mfaApi = new MFAApi(config);
+
+    const message = 'Your temporary {NAME} {SCOPE} code is {CODE}';
+    const digits = 6;
+
+    describe('generateMessagingCode', () => {
+        test('should generate messaging code', async () => {
+            const codeRequest = {
+                to: USER_NUMBER,
+                from: BW_NUMBER,
+                applicationId: BW_MESSAGING_APPLICATION_ID,
+                message: message,
+                digits: digits
+            };
+
+            const { status, data } = await mfaApi.generateMessagingCode(BW_ACCOUNT_ID, codeRequest);
+
+            expect(status).toEqual(200);
+            expect(data.messageId).toHaveLength(29);
+        });
+    });
+
+    describe('generateVoiceCode', () => {
+        test('should generate voice code', async () => {
+            const codeRequest = {
+                to: USER_NUMBER,
+                from: BW_NUMBER,
+                applicationId: BW_VOICE_APPLICATION_ID,
+                message: message,
+                digits: digits
+            };
+
+            const { status, data } = await mfaApi.generateVoiceCode(BW_ACCOUNT_ID, codeRequest);
+
+            expect(status).toEqual(200);
+            expect(data.callId).toHaveLength(47);
+        });
+    });
+
+    describe('verifyCode', () => {
+        test('should verify code', async () => {
+            const verifyRequest = {
+                to: `+1000${Math.floor(Math.random() * 10000000)}`,
+                scope: '2FA',
+                expirationTimeInMinutes: 3,
+                code: '12345'
+            };
+
+            const { status, data } = await mfaApi.verifyCode(BW_ACCOUNT_ID, verifyRequest);
+
+            expect(status).toEqual(200);
+            expect(data.valid).toEqual(false);
+        });
+    });
+});
