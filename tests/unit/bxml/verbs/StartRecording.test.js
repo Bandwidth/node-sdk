4bc6ab542157f67d92be67af4e1535921d1a7a61
ckoegel <ckoegel1006@gmail.com>
2023-10-17 10:24:38 -0400

    finish nn verb tests

---
+++ tests/unit/bxml/verbs/StartRecording.test.js
@@ -0,0 +1,27 @@
+const { Verb } = require('../../../../models/bxml/Verb');
+const { StartRecording } = require('../../../../models/bxml/verbs/StartRecording');
+
+describe('StartRecording', () => {
+    const attributes = {
+        recordingAvailableUrl: 'https://initial.com',
+        recordingAvailableMethod: 'POST',
+        transcribe: true,
+        transcriptionAvailableUrl: 'https://initial.com',
+        transcriptionAvailableMethod: 'POST',
+        username: 'initialUsername',
+        password: 'initialPassword',
+        tag: 'initialTag',
+        fileFormat: 'wav',
+        multiChannel: true
+    };
+
+    const expected = '<StartRecording recordingAvailableUrl="https://initial.com" recordingAvailableMethod="POST" transcribe="true" transcriptionAvailableUrl="https://initial.com" transcriptionAvailableMethod="POST" username="initialUsername" password="initialPassword" tag="initialTag" fileFormat="wav" multiChannel="true"/>';
+
+    test('should create a StartRecording Verb', () => {
+        const startRecording = new StartRecording(attributes);
+
+        expect(startRecording).toBeInstanceOf(StartRecording);
+        expect(startRecording).toBeInstanceOf(Verb);
+        expect(startRecording.toBxml()).toBe(expected);
+    });
+});
