4927fc34f9a6f8dab308a3422e00f4a4f32020c6
ckoegel <ckoegel1006@gmail.com>
2023-10-17 10:07:43 -0400

    spelling in forward test

--- tests/unit/bxml/verbs/Forward.test.js
+++ tests/unit/bxml/verbs/Forward.test.js
@@ -6,12 +6,12 @@ describe('Forward', () => {
         to: '+19195551234',
         from: '+19195554321',
         callTimeout: 5,
-        diversionTreatment: 'propogate',
+        diversionTreatment: 'propagate',
         diversionReason: 'user-busy',
         uui: '93d6f3c0be5845960b744fa28015d8ede84bd1a4;encoding=base64,asdf;encoding=jwt'
     };
 
-    const expected = '<Forward to="+19195551234" from="+19195554321" callTimeout="5" diversionTreatment="propogate" diversionReason="user-busy" uui="93d6f3c0be5845960b744fa28015d8ede84bd1a4;encoding=base64,asdf;encoding=jwt"/>';
+    const expected = '<Forward to="+19195551234" from="+19195554321" callTimeout="5" diversionTreatment="propagate" diversionReason="user-busy" uui="93d6f3c0be5845960b744fa28015d8ede84bd1a4;encoding=base64,asdf;encoding=jwt"/>';
 
     test('should create a Forward Verb', () => {
         const forward = new Forward(attributes);
