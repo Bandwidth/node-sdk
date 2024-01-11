f3911e8ff8427d0a52059612a29e2dcb5b0252de
ckoegel <ckoegel1006@gmail.com>
2024-01-11 17:37:22 -0500

    add tests

--- tests/unit/bxml/Bxml.test.js
+++ tests/unit/bxml/Bxml.test.js
@@ -1,4 +1,5 @@
 const { Bxml } = require('../../../models/bxml/Bxml');
+const { PauseRecording } = require('../../../models/bxml/verbs/PauseRecording');
 const { Root } = require('../../../models/bxml/Root');
 
 describe('Bxml', () => {
@@ -8,6 +9,25 @@ describe('Bxml', () => {
 
         expect(bxml).toBeInstanceOf(Bxml);
         expect(bxml).toBeInstanceOf(Root);
+        expect(bxml.toBxml()).toBe(expected);
+    });
+
+    test('should add a single nested verb', () => {
+        const bxml = new Bxml();
+        const pauseRecording = new PauseRecording();
+        bxml.addVerbs(pauseRecording);
+        const expected = '<?xml version="1.0" encoding="UTF-8"?><Bxml><PauseRecording/></Bxml>';
+
+        expect(bxml.toBxml()).toBe(expected);
+    });
+
+    test('should multiple nested verbs', () => {
+        const bxml = new Bxml();
+        const pauseRecording1 = new PauseRecording();
+        const pauseRecording2 = new PauseRecording();
+        bxml.addVerbs([pauseRecording1, pauseRecording2]);
+        const expected = '<?xml version="1.0" encoding="UTF-8"?><Bxml><PauseRecording/><PauseRecording/></Bxml>';
+
         expect(bxml.toBxml()).toBe(expected);
     });
 });
--- tests/unit/bxml/Response.test.js
+++ tests/unit/bxml/Response.test.js
@@ -1,13 +1,33 @@
 const { Response } = require('../../../models/bxml/Response');
+const { PauseRecording } = require('../../../models/bxml/verbs/PauseRecording');
 const { Root } = require('../../../models/bxml/Root');
 
-describe('Bxml', () => {
-    test('should create a bxml object', () => {
+describe('Response', () => {
+    test('should create a response object', () => {
         const response = new Response();
         const expected = '<?xml version="1.0" encoding="UTF-8"?><Response/>';
 
         expect(response).toBeInstanceOf(Response);
         expect(response).toBeInstanceOf(Root);
+        expect(response.toBxml()).toBe(expected);
+    });
+
+    test('should add a single nested verb', () => {
+        const response = new Response();
+        const pauseRecording = new PauseRecording();
+        response.addVerbs(pauseRecording);
+        const expected = '<?xml version="1.0" encoding="UTF-8"?><Response><PauseRecording/></Response>';
+
+        expect(response.toBxml()).toBe(expected);
+    });
+
+    test('should multiple nested verbs', () => {
+        const response = new Response();
+        const pauseRecording1 = new PauseRecording();
+        const pauseRecording2 = new PauseRecording();
+        response.addVerbs([pauseRecording1, pauseRecording2]);
+        const expected = '<?xml version="1.0" encoding="UTF-8"?><Response><PauseRecording/><PauseRecording/></Response>';
+
         expect(response.toBxml()).toBe(expected);
     });
 });
