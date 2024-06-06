66101f72f687c3d41d85894ddb08bd893c944bba
ckoegel <ckoegel1006@gmail.com>
2024-06-06 14:00:30 -0400

    statistics and transcriptions

--- tests/integration/statistics-api.test.ts
+++ tests/integration/statistics-api.test.ts
@@ -1,13 +1,14 @@
-const { StatisticsApi } = require('../../api/statistics-api');
-const { Configuration } = require("../../configuration");
+//@ts-nocheck
+import { StatisticsApi } from '../../api';
+import { Configuration } from '../../configuration';
 
 describe('StatisticsApi', () => {
-    const config = new Configuration({username: BW_USERNAME, password: BW_PASSWORD});
+    const config = new Configuration({username: globalThis.BW_USERNAME, password: globalThis.BW_PASSWORD});
     const statisticsApi = new StatisticsApi(config);
 
     describe('getStatistics', () => {
         test('should get statistics', async () => {
-            const { status, data } = await statisticsApi.getStatistics(BW_ACCOUNT_ID);
+            const { status, data } = await statisticsApi.getStatistics(globalThis.BW_ACCOUNT_ID);
 
             expect(status).toEqual(200);
             expect(data.currentCallQueueSize).toBeDefined();
@@ -17,22 +18,22 @@ describe('StatisticsApi', () => {
 
     describe('HTTP Errors', () => {
         test('401', async () => {
-            const configBad = new Configuration({username: UNAUTHORIZED_USERNAME, password: UNAUTHORIZED_PASSWORD});
+            const configBad = new Configuration({username: globalThis.UNAUTHORIZED_USERNAME, password: globalThis.UNAUTHORIZED_PASSWORD});
             const statisticsApiBad = new StatisticsApi(configBad);
 
             try {
-                await statisticsApiBad.getStatistics(BW_ACCOUNT_ID);
+                await statisticsApiBad.getStatistics(globalThis.BW_ACCOUNT_ID);
             } catch (e) {
                 expect(e.response.status).toEqual(401);
             }
         });
 
         test('403', async () => {
-            const configBad = new Configuration({username: FORBIDDEN_USERNAME, password: FORBIDDEN_PASSWORD});
+            const configBad = new Configuration({username: globalThis.FORBIDDEN_USERNAME, password: globalThis.FORBIDDEN_PASSWORD});
             const statisticsApiBad = new StatisticsApi(configBad);
 
             try {
-                await statisticsApiBad.getStatistics(BW_ACCOUNT_ID);
+                await statisticsApiBad.getStatistics(globalThis.BW_ACCOUNT_ID);
             } catch (e) {
                 expect(e.response.status).toEqual(403);
             }
--- tests/integration/transcriptions-api.test.ts
+++ tests/integration/transcriptions-api.test.ts
@@ -1,17 +1,17 @@
-const { CallsApi } = require('../../api/calls-api');
-const { TranscriptionsApi } = require('../../api/transcriptions-api');
-const { Configuration } = require('../../configuration');
-const { RecordingStateEnum, CallStateEnum } = require('../../models');
-const { setupManteca, createMantecaCall, cleanupCalls, getMantecaTestStatus, sleep } = require('../callUtils');
+
+import { CallsApi, TranscriptionsApi } from "../../api";
+import { Configuration } from "../../configuration";
+import { CallStateEnum } from "../../models";
+import { cleanupCalls, createMantecaCall, setupManteca, sleep } from "../callUtils";
 
 describe('TranscriptionsApi', () => {
     jest.setTimeout(240000);
-    const config = new Configuration({username: BW_USERNAME, password: BW_PASSWORD});
+    const config = new Configuration({username: globalThis.BW_USERNAME, password: globalThis.BW_PASSWORD});
     const callsApi = new CallsApi(config);
     const transcriptionsApi = new TranscriptionsApi(config);
 
-    let transcriptionId, mantecaTestId, mantecaCallId;
-    let activeCalls = [];
+    let transcriptionId: string, mantecaTestId: string, mantecaCallId: string;
+    let activeCalls: string[] = [];
 
     beforeAll(async () => {
         mantecaTestId = await setupManteca('CALL');
@@ -25,60 +25,60 @@ describe('TranscriptionsApi', () => {
 
     describe('createCallTranscription', () => {
         test('should create call transcription', async () => {
-            await sleep(SLEEP_TIME_S);
+            await sleep(globalThis.SLEEP_TIME_S);
 
             const startTranscriptionBxml =
                 `<?xml version="1.0" encoding="UTF-8"?><Response><StartTranscription name="${mantecaCallId}" tracks="inbound"></StartTranscription><Pause duration="6"/></Response>`;
             const { status: startStatus } =
-                await callsApi.updateCallBxml(BW_ACCOUNT_ID, mantecaCallId, startTranscriptionBxml);
+                await callsApi.updateCallBxml(globalThis.BW_ACCOUNT_ID, mantecaCallId, startTranscriptionBxml);
             expect(startStatus).toEqual(204);
-            await sleep(SLEEP_TIME_S);
+            await sleep(globalThis.SLEEP_TIME_S);
 
             const stopTranscriptionBxml =
                 `<?xml version="1.0" encoding="UTF-8"?><Response><StopTranscription name="${mantecaCallId}"></StopTranscription></Response>`;
             const { status: stopStatus } =
-                await callsApi.updateCallBxml(BW_ACCOUNT_ID, mantecaCallId, stopTranscriptionBxml);
+                await callsApi.updateCallBxml(globalThis.BW_ACCOUNT_ID, mantecaCallId, stopTranscriptionBxml);
             expect(stopStatus).toEqual(204);
-            await sleep(SLEEP_TIME_S);
+            await sleep(globalThis.SLEEP_TIME_S);
 
             const { status: completeStatus } =
-                await callsApi.updateCall(BW_ACCOUNT_ID, mantecaCallId, { state: CallStateEnum.Completed });
+                await callsApi.updateCall(globalThis.BW_ACCOUNT_ID, mantecaCallId, { state: CallStateEnum.Completed });
             expect(completeStatus).toEqual(200);
         });
     });
 
     describe('listCallTranscriptions', () => {
         test('should list call transcriptions', async () => {
-            await sleep(SLEEP_TIME_S * 20);
+            await sleep(globalThis.SLEEP_TIME_S * 20);
 
             const { status, data } =
-                await transcriptionsApi.listRealTimeTranscriptions(BW_ACCOUNT_ID, mantecaCallId);
+                await transcriptionsApi.listRealTimeTranscriptions(globalThis.BW_ACCOUNT_ID, mantecaCallId);
             expect(status).toEqual(200);
             expect(data).toBeInstanceOf(Array);
             expect(typeof data[0].transcriptionId).toBe('string');
             expect(typeof data[0].transcriptionUrl).toBe('string');
-            transcriptionId = data[0].transcriptionId;
+            transcriptionId = data[0].transcriptionId!;
         });
     });
 
     describe('getCallTranscription', () => {
         test('should get call transcription', async () => {
             const { status, data } =
-                await transcriptionsApi.getRealTimeTranscription(BW_ACCOUNT_ID, mantecaCallId, transcriptionId);
+                await transcriptionsApi.getRealTimeTranscription(globalThis.BW_ACCOUNT_ID, mantecaCallId, transcriptionId);
             expect(status).toEqual(200);
-            expect(data.accountId).toEqual(BW_ACCOUNT_ID);
+            expect(data.accountId).toEqual(globalThis.BW_ACCOUNT_ID);
             expect(data.callId).toEqual(mantecaCallId);
             expect(data.transcriptionId).toEqual(transcriptionId);
             expect(data.tracks).toBeInstanceOf(Array);
-            expect(data.tracks[0].track).toEqual('inbound');
-            expect(typeof data.tracks[0].confidence).toBe('number');
+            expect(data.tracks![0].track).toEqual('inbound');
+            expect(typeof data.tracks![0].confidence).toBe('number');
         });
     });
 
     describe('deleteCallTranscription', () => {
         test('should delete call transcription', async () => {
             const { status } =
-                await transcriptionsApi.deleteRealTimeTranscription(BW_ACCOUNT_ID, mantecaCallId, transcriptionId);
+                await transcriptionsApi.deleteRealTimeTranscription(globalThis.BW_ACCOUNT_ID, mantecaCallId, transcriptionId);
             expect(status).toEqual(200); // This is a bug in the API, it should return 204. VAPI-1863 should fix this.
         });
     });
