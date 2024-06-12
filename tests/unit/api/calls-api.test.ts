25c64acecbef926bcc038bc920dc675208757371
ckoegel <ckoegel1006@gmail.com>
2024-06-12 10:32:27 -0400

    add unit tests for calls, conferences, recordings

---
+++ tests/unit/api/calls-api.test.ts
@@ -0,0 +1,205 @@
+//@ts-nocheck
+import { CallsApi } from '../../../api';
+import { Configuration } from '../../../configuration';
+import {
+    CallbackMethodEnum,
+    CallDirectionEnum,
+    CallStateEnum,
+    CreateCall,
+    MachineDetectionConfiguration,
+    UpdateCall
+} from '../../../models';
+
+describe('CallsApi', () => {
+    const config = new Configuration({
+        username: globalThis.BW_USERNAME,
+        password: globalThis.BW_PASSWORD,
+        basePath: 'http://127.0.0.1:4010'
+    });
+    const callsApi = new CallsApi(config);
+
+    const callId = 'c-1234';
+    const displayName = 'NodeJS SDK';
+    const answerMethod = CallbackMethodEnum.Post;
+    const answerUrl = `${globalThis.BASE_CALLBACK_URL}/callbacks/answer`;
+    const disconnectMethod = CallbackMethodEnum.Get;
+    const disconnectUrl = `${globalThis.BASE_CALLBACK_URL}/callbacks/disconnect`;
+    const callTimeout = 30.0;
+    const callbackTimeout = 15.0;
+
+    describe('createCall', () => {
+        test('should create a call', async () => {
+            const amdConfig: MachineDetectionConfiguration = {
+                mode: 'async',
+                detectionTimeout: 5.0,
+                silenceTimeout: 5.0,
+                speechThreshold: 5.0,
+                speechEndThreshold: 5.0,
+                delayResult: true,
+                callbackUrl: globalThis.BASE_CALLBACK_URL + '/machineDetection',
+                callbackMethod: CallbackMethodEnum.Post
+            };
+
+            const callBody: CreateCall = {
+                applicationId: globalThis.BW_VOICE_APPLICATION_ID,
+                to: globalThis.USER_NUMBER,
+                from: globalThis.BW_NUMBER,
+                privacy: false,
+                displayName: displayName,
+                answerUrl: answerUrl,
+                answerMethod: answerMethod,
+                disconnectUrl: disconnectUrl,
+                disconnectMethod: disconnectMethod,
+                machineDetection: amdConfig,
+                callTimeout: callTimeout,
+                callbackTimeout: callbackTimeout
+            };
+
+            const { status, data } = await callsApi.createCall(globalThis.BW_ACCOUNT_ID, callBody);
+
+            expect(status).toEqual(201);
+            expect(data.applicationId).toHaveLength(36);
+            expect(data.accountId).toHaveLength(7);
+            expect(data.callId).toHaveLength(47);
+            expect(data.to).toHaveLength(12);
+            expect(data.from).toHaveLength(12);
+            expect(data.enqueuedTime).toBeDateString();
+            expect(data.callUrl).toStartWith('http');
+            expect(data.callTimeout).toBePositive();
+            expect(data.callbackTimeout).toBePositive();
+            expect(data.tag).toBeString();
+            expect(data.answerMethod).toBeOneOf([CallbackMethodEnum.Post, CallbackMethodEnum.Get]);
+            expect(data.answerUrl).toStartWith('http');
+            expect(data.answerFallbackMethod).toBeOneOf([CallbackMethodEnum.Post, CallbackMethodEnum.Get]);
+            expect(data.answerFallbackUrl).toStartWith('http');
+            expect(data.disconnectMethod).toBeOneOf([CallbackMethodEnum.Post, CallbackMethodEnum.Get]);
+            expect(data.disconnectUrl).toStartWith('http');
+            expect(data.username).toBeString();
+            expect(data.password).toBeString();
+            expect(data.fallbackUsername).toBeString();
+            expect(data.fallbackPassword).toBeString();
+            expect(data.priority).toBePositive();
+        });
+    });
+
+    describe('getCall', () => {
+        test('should return a call', async () => {
+            const { status, data } = await callsApi.getCallState(globalThis.BW_ACCOUNT_ID, callId);
+
+            expect(status).toEqual(200);
+            expect(data.applicationId).toHaveLength(36);
+            expect(data.accountId).toHaveLength(7);
+            expect(data.callId).toHaveLength(47);
+            expect(data.parentCallId).toHaveLength(47);
+            expect(data.to).toHaveLength(12);
+            expect(data.from).toHaveLength(12);
+            expect(data.direction).toBeOneOf([CallDirectionEnum.Inbound, CallDirectionEnum.Outbound]);
+            expect(data.state).toBeString();
+            expect(data.stirShaken).toBeObject();
+            expect(data.identity).toBeString();
+            expect(data.enqueuedTime).toBeDateString();
+            expect(data.startTime).toBeDateString();
+            expect(data.answerTime).toBeDateString();
+            expect(data.endTime).toBeDateString();
+            expect(data.disconnectCause).toBeString();
+            expect(data.errorMessage).toBeDefined();
+            expect(data.errorId).toBeDefined();
+            expect(data.lastUpdate).toBeDateString();
+        });
+    });
+    
+    describe('listCalls', () => {
+        test('should return an array of calls', async () => {
+            const { status, data } = await callsApi.listCalls(globalThis.BW_ACCOUNT_ID);
+
+            expect(status).toEqual(200);
+            expect(data).toBeInstanceOf(Array);
+            expect(data[0].applicationId).toHaveLength(36);
+            expect(data[0].accountId).toHaveLength(7);
+            expect(data[0].callId).toHaveLength(47);
+            expect(data[0].parentCallId).toHaveLength(47);
+            expect(data[0].to).toHaveLength(12);
+            expect(data[0].from).toHaveLength(12);
+            expect(data[0].direction).toBeOneOf([CallDirectionEnum.Inbound, CallDirectionEnum.Outbound]);
+            expect(data[0].state).toBeString();
+            expect(data[0].stirShaken).toBeObject();
+            expect(data[0].identity).toBeString();
+            expect(data[0].enqueuedTime).toBeDateString();
+            expect(data[0].startTime).toBeDateString();
+            expect(data[0].answerTime).toBeDateString();
+            expect(data[0].endTime).toBeDateString();
+            expect(data[0].disconnectCause).toBeString();
+            expect(data[0].errorMessage).toBeDefined();
+            expect(data[0].errorId).toBeDefined();
+            expect(data[0].lastUpdate).toBeDateString();
+        });
+    });
+
+    describe('updateCall', () => {
+        test('should update a call', async () => {
+            const updateCallBody: UpdateCall = {
+                state: CallStateEnum.Active,
+                redirectUrl: `${globalThis.MANTECA_BASE_URL}/bxml/pause`
+            };
+
+            const { status } = await callsApi.updateCall(globalThis.BW_ACCOUNT_ID, callId, updateCallBody);
+            expect(status).toEqual(200);
+        });
+    });
+
+    describe('updateCallBxml', () => {
+        test('should update a call with bxml', async () => {
+            const updateBxml = '<?xml version="1.0" encoding="UTF-8"?><Bxml><SpeakSentence locale="en_US" gender="female" voice="susan">This is a test bxml response</SpeakSentence><Pause duration="3"/></Bxml>';
+
+            const { status } = await callsApi.updateCallBxml(globalThis.BW_ACCOUNT_ID, callId, updateBxml);
+            expect(status).toEqual(204);
+        });
+    });
+
+    describe('HTTP Errors', () => {
+        test('400', async () => {
+            const callBodyBad: CreateCall = {
+                applicationId: globalThis.BW_VOICE_APPLICATION_ID,
+                to: '+1invalid',
+                from: globalThis.BW_NUMBER,
+            };
+
+            try {
+                await callsApi.createCall(globalThis.BW_ACCOUNT_ID, callBodyBad);
+            } catch (e) {
+                expect(e.response.status).toEqual(400);
+            }
+        });
+
+        test('401', async () => {
+            const configBad = new Configuration({username: globalThis.UNAUTHORIZED_USERNAME, password: globalThis.UNAUTHORIZED_PASSWORD});
+            const callsApiBad = new CallsApi(configBad);
+
+            try {
+                await callsApiBad.getCallState(globalThis.BW_ACCOUNT_ID, callId);
+            } catch (e) {
+                expect(e.response.status).toEqual(401);
+            }
+        });
+
+        test('403', async () => {
+            const configBad = new Configuration({username: globalThis.FORBIDDEN_USERNAME, password: globalThis.FORBIDDEN_PASSWORD});
+            const callsApiBad = new CallsApi(configBad);
+
+            try {
+                await callsApiBad.getCallState(globalThis.BW_ACCOUNT_ID, callId);
+            } catch (e) {
+                expect(e.response.status).toEqual(403);
+            }
+        });
+
+        test('404', async () => {
+            try {
+                await callsApi.getCallState(globalThis.BW_ACCOUNT_ID, 'does-not-exist');
+            } catch (e) {
+                expect(e.response.status).toEqual(404);
+            }
+        });
+    });
+
+});
---
+++ tests/unit/api/conferences-api.test.ts
@@ -0,0 +1,160 @@
+//@ts-nocheck
+import { ConferencesApi } from '../../../api';
+import { Configuration } from '../../../configuration';
+import {
+    ConferenceStateEnum,
+    RedirectMethodEnum,
+    FileFormatEnum,
+    UpdateConference,
+    CallbackMethodEnum
+} from '../../../models';
+
+describe('ConferencesApi', () => {
+    const config = new Configuration({
+        username: globalThis.BW_USERNAME,
+        password: globalThis.BW_PASSWORD,
+        basePath: 'http://127.0.0.1:4010'
+    });
+    const conferencesApi = new ConferencesApi(config);
+
+    const callId = 'c-1234';
+    const conferenceId = 'c-4321';
+    const recordingId = 'r-1234';
+    
+    describe('listConferences', () => {
+        test('should list conferences', async () => {
+            const { status, data } = await conferencesApi.listConferences(globalThis.BW_ACCOUNT_ID, conferenceId);
+
+            expect(status).toEqual(200);
+            expect(data).toBeInstanceOf(Array);
+            expect(data[0].id).toHaveLength(50);
+            expect(data[0].name).toBeString();
+            expect(data[0].createdTime).toBeDateString();
+            expect(data[0].completedTime).toBeDateString();
+            expect(data[0].conferenceEventUrl).toStartWith('http');
+            expect(data[0].conferenceEventMethod).toBeOneOf([CallbackMethodEnum.Post, CallbackMethodEnum.Get]);
+            expect(data[0].tag).toBeString();
+        });
+    });
+
+    describe('getConference', () => {
+        test('should get a conference', async () => {
+            const { status, data } = await conferencesApi.getConference(globalThis.BW_ACCOUNT_ID, conferenceId);
+    
+            expect(status).toEqual(200);
+            expect(data.id).toHaveLength(50);
+            expect(data.name).toBeString();
+            expect(data.createdTime).toBeDateString();
+            expect(data.completedTime).toBeDateString();
+            expect(data.conferenceEventUrl).toStartWith('http');
+            expect(data.conferenceEventMethod).toBeOneOf([CallbackMethodEnum.Post, CallbackMethodEnum.Get]);
+            expect(data.tag).toBeString();
+        });
+    });
+
+    describe('getConferenceMember', () => {
+        test('should get conference member', async () => {
+            const { status, data } = await conferencesApi.getConferenceMember(globalThis.BW_ACCOUNT_ID, conferenceId, callId);
+    
+            expect(status).toEqual(200);
+            expect(data.callId).toHaveLength(47);
+            expect(data.conferenceId).toHaveLength(50);
+            expect(data.memberUrl).toStartWith('http');
+            expect(data.mute).toBeBoolean();
+            expect(data.hold).toBeBoolean();
+            expect(data.callIdsToCoach).toBeArray();
+        });
+    });
+
+    describe('updateConferenceMember', () => {
+        test('should update conference member', async () => {
+            const updateConferenceMember = { mute: false };
+
+            const { status } =
+                await conferencesApi.updateConferenceMember(globalThis.BW_ACCOUNT_ID, conferenceId, callId, updateConferenceMember);
+    
+            expect(status).toEqual(204);
+        });
+    });
+
+    describe('updateConference', () => {
+        test('should update conference', async () => {
+            const updateConferenceBody: UpdateConference = {
+                status: ConferenceStateEnum.Active,
+                redirectUrl: `${globalThis.MANTECA_BASE_URL}/bxml/pause`,
+                redirectMethod: RedirectMethodEnum.Post,
+                username: 'username',
+                password: 'password',
+                redirectFallbackUrl: `${globalThis.MANTECA_BASE_URL}/bxml/pause`,
+                redirectFallbackMethod: RedirectMethodEnum.Post,
+                fallbackUsername: 'username',
+                fallbackPassword: 'password'
+            };
+
+            const { status } =
+                await conferencesApi.updateConference(globalThis.BW_ACCOUNT_ID, conferenceId, updateConferenceBody);
+    
+            expect(status).toEqual(204);
+        });
+    });
+
+    describe('updateConferenceBxml', () => {
+        test('should update conference bxml', async () => {
+            const updateBxml = '<?xml version="1.0" encoding="UTF-8"?><Bxml><StartRecording/><SpeakSentence locale="en_US" gender="female" voice="susan">This should be a conference recording.</SpeakSentence><StopRecording/></Bxml>';
+
+            const { status } =
+                await conferencesApi.updateConferenceBxml(globalThis.BW_ACCOUNT_ID, conferenceId, updateBxml);
+    
+            expect(status).toEqual(204);
+        });
+    });
+
+    describe('listConferenceRecordings', () => {
+        test('should list conference recordings', async () => {
+            const { status, data } = await conferencesApi.listConferenceRecordings(globalThis.BW_ACCOUNT_ID, conferenceId);
+    
+            expect(status).toEqual(200);
+            expect(data).toBeInstanceOf(Array);
+            expect(data[0].accountId).toHaveLength(7);
+            expect(data[0].conferenceId).toHaveLength(50);
+            expect(data[0].name).toBeString();
+            expect(data[0].recordingId).toHaveLength(47);
+            expect(data[0].duration).toStartWith('PT');
+            expect(data[0].channels).toBeNumber();
+            expect(data[0].startTime).toBeDateString();
+            expect(data[0].endTime).toBeDateString();
+            expect(data[0].fileFormat).toBeOneOf([FileFormatEnum.Wav, FileFormatEnum.Mp3]);
+            expect(data[0].status).toBeString();
+            expect(data[0].mediaUrl).toStartWith('http');
+        });
+    });
+
+    describe('getConferenceRecording', () => {
+        test('should get conference recording', async () => {
+            const { status, data } = await conferencesApi.getConferenceRecording(globalThis.BW_ACCOUNT_ID, conferenceId, recordingId);
+    
+            expect(status).toEqual(200);
+            expect(data.accountId).toHaveLength(7);
+            expect(data.conferenceId).toHaveLength(50);
+            expect(data.name).toBeString();
+            expect(data.recordingId).toHaveLength(47);
+            expect(data.duration).toStartWith('PT');
+            expect(data.channels).toBeNumber();
+            expect(data.startTime).toBeDateString();
+            expect(data.endTime).toBeDateString();
+            expect(data.fileFormat).toBeOneOf([FileFormatEnum.Wav, FileFormatEnum.Mp3]);
+            expect(data.status).toBeString();
+            expect(data.mediaUrl).toStartWith('http');
+        });
+    });
+
+    describe('downloadConferenceRecording', () => {
+        test('should download conference recording', async () => {
+            const { status, data } = await conferencesApi.downloadConferenceRecording(globalThis.BW_ACCOUNT_ID, conferenceId, recordingId);
+
+            expect(status).toEqual(200);
+            expect(data).toBeDefined();
+        });
+    });
+
+});
