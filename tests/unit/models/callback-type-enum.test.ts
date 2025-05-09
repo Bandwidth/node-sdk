65236f268db988a889d68c11612e9fa39afd8e71
ckoegel <ckoegel1006@gmail.com>
2025-05-09 17:40:54 -0400

    update callback type enum (breaking)

--- tests/unit/models/callback-type-enum.test.ts
+++ tests/unit/models/callback-type-enum.test.ts
@@ -2,9 +2,9 @@ import { CallbackTypeEnum } from '../../../models/callback-type-enum';
 
 describe('CallbackTypeEnum', () => {
     test('should define the expected values', () => {
-        expect(CallbackTypeEnum.Received).toBe('message-received');
-        expect(CallbackTypeEnum.Delivered).toBe('message-delivered');
-        expect(CallbackTypeEnum.Failed).toBe('message-failed');
-        expect(CallbackTypeEnum.Sending).toBe('message-sending');
+        expect(CallbackTypeEnum.MessageReceived).toBe('message-received');
+        expect(CallbackTypeEnum.MessageDelivered).toBe('message-delivered');
+        expect(CallbackTypeEnum.MessageFailed).toBe('message-failed');
+        expect(CallbackTypeEnum.MessageSending).toBe('message-sending');
     });
 });
