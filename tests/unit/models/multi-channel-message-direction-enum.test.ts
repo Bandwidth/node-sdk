72bad1b5e8ee5a82cf34051667a7555b8095bf71
ckoegel <ckoegel1006@gmail.com>
2025-07-31 16:37:00 -0400

    remove old file

--- tests/unit/models/multi-channel-message-direction-enum.test.ts
+++
@@ -1,8 +0,0 @@
-import { MultiChannelMessageDirectionEnum } from '../../../models/multi-channel-message-direction-enum';
-
-describe('MultiChannelMessageDirectionEnum', () => {
-    test('should define the expected values', () => {
-        expect(MultiChannelMessageDirectionEnum.Inbound).toBe('INBOUND');
-        expect(MultiChannelMessageDirectionEnum.Outbound).toBe('OUTBOUND');
-    });
-});
