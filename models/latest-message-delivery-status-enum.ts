24372e282acc2549494321588e874435f41101d1
DX-Bandwidth <dx@bandwidth.com>
2026-03-18 20:08:38 +0000

    Generate SDK with OpenAPI Generator Version

--- models/latest-message-delivery-status-enum.ts
+++ models/latest-message-delivery-status-enum.ts
@@ -15,13 +15,14 @@
 
 
 /**
- * [DNI-Only](#section/DNI-Only). The current delivery status of the phone number. - ACTIVE: A message was successfully sent to the number (delivery code 0). - DEACTIVATED: A message was not delivered to a number (delivery code 720) - UNKNOWN: Bandwidth cannot find an delivery status entry for the number.
+ * [DNI-Only](#section/DNI-Only). The current delivery status of the phone number. - ACTIVE: A message was successfully sent to the number (delivery code 0). - DEACTIVATED: A message was not delivered to a number (delivery code 720) - UNKNOWN: Bandwidth cannot find an delivery status entry for the number. - NOT_ENABLED: The phone number is not enabled for messaging, so there is no delivery status information.
  */
 
 export const LatestMessageDeliveryStatusEnum = {
     Active: 'ACTIVE',
     Deactivated: 'DEACTIVATED',
-    Unknown: 'UNKNOWN'
+    Unknown: 'UNKNOWN',
+    NotEnabled: 'NOT_ENABLED'
 } as const;
 
 export type LatestMessageDeliveryStatusEnum = typeof LatestMessageDeliveryStatusEnum[keyof typeof LatestMessageDeliveryStatusEnum];
