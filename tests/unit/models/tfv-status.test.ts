c7b3837261912c5e9c4276fc1efb94a9014a3b8c
ckoegel <ckoegel1006@gmail.com>
2026-06-18 13:51:57 -0400

    add unit test

--- tests/unit/models/tfv-status.test.ts
+++ tests/unit/models/tfv-status.test.ts
@@ -1,14 +1,22 @@
 import { TfvStatus } from '../../../models/tfv-status';
 import { TfvStatusEnum } from '../../../models/tfv-status-enum';
 import { TfvSubmissionInfo } from '../../../models/tfv-submission-info';
+import { AdditionalDenialReason } from '../../../models/additional-denial-reason';
 
 describe('TfvStatus', () => {
     test('should accept the expected shape', () => {
+        const denialReason: AdditionalDenialReason = {
+                    statusCode: 1.5,
+                    reason: 'test-reason',
+                    resubmitAllowed: true,
+                };
         const fixture: TfvStatus = {
             phoneNumber: 'test-phoneNumber',
             status: TfvStatusEnum.Verified,
             internalTicketNumber: 'test-internalTicketNumber',
             declineReasonDescription: 'test-declineReasonDescription',
+            denialStatusCode: 123,
+            additionalDenialReasons: [denialReason],
             resubmitAllowed: true,
             createdDateTime: 'test-createdDateTime',
             modifiedDateTime: 'test-modifiedDateTime',
