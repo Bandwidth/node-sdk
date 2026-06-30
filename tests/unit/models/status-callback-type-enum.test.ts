f333598a9d5bac79f5dabc5e16de4e1b92a40f39
ckoegel <ckoegel1006@gmail.com>
2026-06-30 16:37:35 -0400

    unit test

--- tests/unit/models/status-callback-type-enum.test.ts
+++ tests/unit/models/status-callback-type-enum.test.ts
@@ -3,11 +3,13 @@ import { StatusCallbackTypeEnum } from '../../../models/status-callback-type-enu
 describe('StatusCallbackTypeEnum', () => {
     test('should define the expected values', () => {
         expect(StatusCallbackTypeEnum.MessageSending).toBe('message-sending');
+        expect(StatusCallbackTypeEnum.MessageDelivered).toBe('message-sent');
         expect(StatusCallbackTypeEnum.MessageDelivered).toBe('message-delivered');
         expect(StatusCallbackTypeEnum.MessageFailed).toBe('message-failed');
         expect(StatusCallbackTypeEnum.MessageRead).toBe('message-read');
         expect(Object.values(StatusCallbackTypeEnum)).toEqual([
             'message-sending',
+            'message-sent',
             'message-delivered',
             'message-failed',
             'message-read'
